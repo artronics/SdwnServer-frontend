9076e3328a3246c9d7939c6ec1834ff4879306db
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-01 17:34:35 +0100

    delete comment

--- app/scripts/states.js
+++ app/scripts/states.js
@@ -38,20 +38,6 @@ angular
         // Home //
         //////////
 
-        //.state('home', {
-        //
-        //  // Use a url of "/" to set a states as the "index".
-        //  url: '/',
-        //
-        //  // Example of an inline template string. By default, templates
-        //  // will populate the ui-view within the parent state's template.
-        //  // For top level states, like this one, the parent template is
-        //  // the index.html file. So this template will be inserted into the
-        //  // ui-view within index.html.
-        //  templateUrl: 'views/layout.html'
-        //}, 100)
-
-
         .state('app', {
           url: '/app',
           templateUrl:'views/layout.html',
